b8e739db04605dfb5d123feddddcdace35fd656a
erri120 <erri120@protonmail.com>
2020-07-11 12:11:20 +0200

    Created Unit Test

---
+++ DLSiteMetadata.Test/TestDLSiteGame.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using Playnite.SDK;
+using Xunit;
+
+namespace DLSiteMetadata.Test
+{
+    public class TestDLSiteGame : IClassFixture<TestLoggerFixture>
+    {
+        private readonly ILogger _logger;
+
+        public TestDLSiteGame(TestLoggerFixture fixture)
+        {
+            _logger = fixture.Logger;
+        }
+
+        [Fact]
+        public async Task TestLoadENGGame()
+        {
+            //https://www.dlsite.com/ecchi-eng/work/=/product_id/RE234198.html
+            var game = await DLSiteGame.LoadGame("RE234198", _logger);
+            
+            Assert.NotNull(game);
+            Assert.NotNull(game.DLSiteLink);
+            Assert.NotNull(game.Name);
+            Assert.NotNull(game.Description);
+            Assert.NotNull(game.Circle);
+            Assert.NotNull(game.CircleLink);
+            Assert.NotNull(game.Genres);
+            Assert.NotEmpty(game.Genres);
+            Assert.NotNull(game.ImageURLs);
+            Assert.NotEmpty(game.ImageURLs);
+        }
+
+        [Fact]
+        public async Task TestLoadJPNGame()
+        {
+            //https://www.dlsite.com/maniax/work/=/product_id/RJ173356.html
+            var game = await DLSiteGame.LoadGame("RJ173356", _logger);
+
+            Assert.NotNull(game);
+            Assert.NotNull(game.DLSiteLink);
+            Assert.NotNull(game.Name);
+            Assert.NotNull(game.Description);
+            Assert.NotNull(game.Circle);
+            Assert.NotNull(game.CircleLink);
+            Assert.NotNull(game.Genres);
+            Assert.NotEmpty(game.Genres);
+            Assert.NotNull(game.ImageURLs);
+            Assert.NotEmpty(game.ImageURLs);
+        }
+    }
+}
---
+++ DLSiteMetadata.Test/TestLoggerFixture.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Playnite.SDK;
+
+namespace DLSiteMetadata.Test
+{
+    public class TestLoggerFixture : IDisposable
+    {
+        public readonly TestLogger Logger;
+
+        public TestLoggerFixture()
+        {
+            Logger = new TestLogger();
+        }
+
+        public void Dispose() { }
+    }
+
+    public class TestLogger : ILogger
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine($"[INFO]{message}");
+        }
+
+        public void Info(Exception exception, string message)
+        {
+            Console.WriteLine($"[INFO]{message}");
+            throw new Exception(message);
+        }
+
+        public void Debug(string message)
+        {
+            Console.WriteLine($"[DEBUG]{message}");
+        }
+
+        public void Debug(Exception exception, string message)
+        {
+            Console.WriteLine($"[DEBUG]{message}");
+            throw new Exception(message);
+        }
+
+        public void Warn(string message)
+        {
+            Console.WriteLine($"[WARN]{message}");
+        }
+
+        public void Warn(Exception exception, string message)
+        {
+            Console.WriteLine($"[WARN]{message}");
+            throw new Exception(message);
+        }
+
+        public void Error(string message)
+        {
+            Console.WriteLine($"[ERROR]{message}");
+            throw new Exception(message);
+        }
+
+        public void Error(Exception exception, string message)
+        {
+            Console.WriteLine($"[ERROR]{message}");
+            throw exception;
+        }
+    }
+}
