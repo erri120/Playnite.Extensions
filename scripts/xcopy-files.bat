2fc4564e60df5aa1d240470edbf0d444541cd858
erri120 <erri120@protonmail.com>
2020-08-13 16:53:57 +0200

    Updated scripts

--- scripts/xcopy-files.bat
+++ scripts/xcopy-files.bat
@@ -1,12 +1,29 @@
-Rem ./xcopy-files.bat "M:\Projects\Playnite.Extensions\DLSiteMetadata\bin\Debug" "M:\Games\Playnite\Extensions\DLSiteMetadata"
+@echo off
+Rem ./xcopy-files.bat "M:\Games\Playnite\Extensions"
 
-set inputFolder=%1
-set outputFolder=%2
+set outputFolder=%1
 
-echo %inputFolder%
 echo %outputFolder%
 
-xcopy %inputFolder%\*.dll %outputFolder% /Y
-xcopy %inputFolder%\*.pdb %outputFolder% /Y
-xcopy %inputFolder%\extension.yaml %outputFolder% /Y
-xcopy %inputFolder%\icon.png %outputFolder% /Y
+set len=6
+set obj[0]=DLSiteMetadata
+set obj[1]=ExtensionsUpdater
+set obj[2]=F95ZoneMetadata
+set obj[3]=JastusaMetadata
+set obj[4]=VNDBMetadata
+set obj[5]=VNDBMetadata
+
+set i=0
+:loop
+if %i% equ %len% goto :eof
+for /f "usebackq delims== tokens=2" %%j in (`set obj[%i%]`) do (
+    set extensionPath=%cd%\%%j\bin\Debug
+    set outPath=%outputFolder%\%%j
+    echo %i%: %extensionPath% to %outPath%
+    xcopy %extensionPath%\*.dll %outPath% /Y
+    xcopy %extensionPath%\*.pdb %outPath% /Y
+    xcopy %extensionPath%\extension.yaml %outPath% /Y
+    xcopy %extensionPath%\icon.png %outPath% /Y
+)
+set /a i=%i%+1
+goto loop
