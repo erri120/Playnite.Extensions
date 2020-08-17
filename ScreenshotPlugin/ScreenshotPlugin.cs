fa506ec81166cc02648ffde161803aa4ddb79d7e
erri120 <erri120@protonmail.com>
2020-08-17 12:07:47 +0200

    Created ScreenshotPlugin

---
+++ ScreenshotPlugin/ScreenshotPlugin.cs
@@ -0,0 +1,58 @@
+﻿// /*
+//     Copyright (C) 2020  erri120
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// */
+
+using System;
+using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.SDK.Plugins;
+
+namespace ScreenshotPlugin
+{
+    public class ScreenshotPlugin : Plugin
+    {
+        private readonly IPlayniteAPI _playniteAPI;
+        private readonly ILogger _logger;
+        
+        public override Guid Id { get; } = Guid.Parse("2ace02d2-1f1d-4d11-b430-63d7613eaa1f");
+
+        public ScreenshotPlugin(IPlayniteAPI playniteAPI) : base(playniteAPI)
+        {
+            _playniteAPI = playniteAPI;
+            _logger = playniteAPI.CreateLogger();
+        }
+
+        public override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+        }
+
+        public override void OnApplicationStopped()
+        {
+            base.OnApplicationStopped();
+        }
+
+        public override void OnGameStarted(Game game)
+        {
+            base.OnGameStarted(game);
+        }
+
+        public override void OnGameStopped(Game game, long ellapsedSeconds)
+        {
+            base.OnGameStopped(game, ellapsedSeconds);
+        }
+    }
+}
--- scripts/xcopy-files.bat
+++ scripts/xcopy-files.bat
@@ -5,13 +5,14 @@ set outputFolder=%1
 
 echo %outputFolder%
 
-set len=6
+set len=7
 set obj[0]=DLSiteMetadata
 set obj[1]=ExtensionUpdater
 set obj[2]=F95ZoneMetadata
 set obj[3]=JastusaMetadata
 set obj[4]=VNDBMetadata
-set obj[5]=VNDBMetadata
+set obj[5]=ScreenshotPlugin
+set obj[6]=ScreenshotPlugin
 
 set i=0
 :loop
@@ -20,10 +21,10 @@ for /f "usebackq delims== tokens=2" %%j in (`set obj[%i%]`) do (
     set extensionPath=%cd%\%%j\bin\Debug
     set outPath=%outputFolder%\%%j
     echo %i%: %extensionPath% to %outPath%
-    xcopy %extensionPath%\*.dll %outPath% /Y
-    xcopy %extensionPath%\*.pdb %outPath% /Y
-    xcopy %extensionPath%\extension.yaml %outPath% /Y
-    xcopy %extensionPath%\icon.png %outPath% /Y
+    xcopy %extensionPath%\*.dll %outPath%\ /Y
+    xcopy %extensionPath%\*.pdb %outPath%\ /Y
+    xcopy %extensionPath%\extension.yaml %outPath%\ /Y
+    xcopy %extensionPath%\icon.png %outPath%\ /Y
 )
 set /a i=%i%+1
 goto loop
