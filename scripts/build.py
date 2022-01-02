95c377d4d8b659cba6450fd2fa3e729ef783e041
erri120 <erri120@protonmail.com>
2022-01-02 14:02:19 +0100

    Update script

--- scripts/build.py
+++ scripts/build.py
@@ -2,8 +2,8 @@ from encodings import utf_8
 import sys
 import errno
 import os
-import subprocess
 import shutil
+import zipfile
 
 configuration = "Debug"
 target = "net462"
@@ -32,36 +32,39 @@ def get_build_output_path(plugin_path: str) -> str:
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         raise ValueError("Not enough arguments!")
 
-    output_path = os.path.abspath(sys.argv[1])
+    output_path = os.path.abspath(sys.argv[2])
     validate_path(output_path)
 
     src_path = os.path.abspath("../src")
     validate_path(src_path)
 
+    mode = sys.argv[1]
+
     for plugin in plugins:
         plugin_path = os.path.join(src_path, plugin)
         validate_path(plugin_path)
 
-#         print(f"Building project {plugin}")
-#
-#         process_result = subprocess.run("dotnet build", shell=True, capture_output=True, check=False, cwd=plugin_path)
-#         print(decode_utf8(process_result.stderr))
-#         print(decode_utf8(process_result.stdout))
-#         process_result.check_returncode()
-
         build_output_path = get_build_output_path(plugin_path)
-        plugin_output_path = os.path.join(output_path, plugin)
 
-        if os.path.exists(plugin_output_path):
-            shutil.rmtree(plugin_output_path)
+        if mode == "copy":
+            plugin_output_path = os.path.join(output_path, plugin)
 
-        print(f"Copying build output files from {build_output_path} to {plugin_output_path}")
-        shutil.copytree(build_output_path, plugin_output_path)
+            if os.path.exists(plugin_output_path):
+                shutil.rmtree(plugin_output_path)
 
-    pass
+            print(f"Copying build output files from {build_output_path} to {plugin_output_path}")
+            shutil.copytree(build_output_path, plugin_output_path)
+        elif mode == "pack":
+            zip_output_path = os.path.join(output_path, f"{plugin}.zip")
+            with zipfile.ZipFile(zip_output_path, "w", zipfile.ZIP_LZMA) as myzip:
+                for root, dirs, files in os.walk(build_output_path):
+                    for file in files:
+                        myzip.write(os.path.join(root, file), file)
+        else:
+            raise ValueError(f"Unknown mode: {mode}")
 
 
 if __name__ == "__main__":
