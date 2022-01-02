871da2e714e09c916e0640aec551015950a82652
erri120 <erri120@protonmail.com>
2022-01-02 14:24:18 +0100

    Add CHANGELOG and update script

--- scripts/build.py
+++ scripts/build.py
@@ -35,9 +35,6 @@ def main():
     if len(sys.argv) != 3:
         raise ValueError("Not enough arguments!")
 
-    output_path = os.path.abspath(sys.argv[2])
-    validate_path(output_path)
-
     src_path = os.path.abspath("../src")
     validate_path(src_path)
 
@@ -50,6 +47,9 @@ def main():
         build_output_path = get_build_output_path(plugin_path)
 
         if mode == "copy":
+            output_path = os.path.abspath(sys.argv[2])
+            validate_path(output_path)
+
             plugin_output_path = os.path.join(output_path, plugin)
 
             if os.path.exists(plugin_output_path):
@@ -58,11 +58,31 @@ def main():
             print(f"Copying build output files from {build_output_path} to {plugin_output_path}")
             shutil.copytree(build_output_path, plugin_output_path)
         elif mode == "pack":
+            output_path = os.path.abspath(sys.argv[2])
+            validate_path(output_path)
+
             zip_output_path = os.path.join(output_path, f"{plugin}.zip")
             with zipfile.ZipFile(zip_output_path, "w", zipfile.ZIP_LZMA) as myzip:
                 for root, dirs, files in os.walk(build_output_path):
                     for file in files:
                         myzip.write(os.path.join(root, file), file)
+        elif mode == "update":
+            new_version = sys.argv[2]
+
+            extension_file = os.path.join(build_output_path, "extension.yaml")
+            validate_path(extension_file, False)
+
+            lines = []
+            with open(extension_file, "r", encoding="UTF-8") as f:
+                for line in f:
+                    if line.startswith("Version: "):
+                        lines.append(f"Version: {new_version}\n")
+                    else:
+                        lines.append(line)
+
+            with open(extension_file, "w", encoding="UTF-8") as f:
+                f.writelines(lines)
+
         else:
             raise ValueError(f"Unknown mode: {mode}")
 
