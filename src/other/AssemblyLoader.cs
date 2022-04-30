04fdb997d89a59ac3eb0207faaed44c55d5870d5
erri120 <erri120@protonmail.com>
2022-04-30 11:58:15 +0200

    Add more logging to the Assembly Loader

--- src/other/AssemblyLoader.cs
+++ src/other/AssemblyLoader.cs
@@ -25,6 +25,7 @@ internal static class AssemblyLoader
             // the referenced Assembly was already loaded so no need to do anything
             if (loadedAssembly is not null)
             {
+                logger.LogDebug("Referenced Assembly has already been loaded: referenced: \"{ReferencedAssemblyName}\" loaded: \"{LoadedAssemblyName}\"", referencedAssemblyName.FullName, loadedAssembly.FullName);
                 ValidateAssemblyVersions(logger, referencedAssemblyName, loadedAssembly.GetName());
                 continue;
             }
@@ -39,20 +40,21 @@ internal static class AssemblyLoader
                 }
                 catch (Exception e)
                 {
-                    logger.LogError(e, "Unable to load Assembly {AssemblyFullName} from {Path}", referencedAssemblyName.FullName, dllPath);
+                    logger.LogError(e, "Unable to load Assembly \"{AssemblyFullName}\" from path \"{Path}\"", referencedAssemblyName.FullName, dllPath);
                     loadedAssembly = null;
                 }
 
                 // we successfully loaded the referenced Assembly from file
                 if (loadedAssembly is not null)
                 {
+                    logger.LogDebug("Referenced Assembly got loaded from disk: referenced: \"{ReferencedAssemblyName}\" loaded: \"{LoadedAssemblyName}\" path: \"{Path}\"", referencedAssemblyName.FullName, loadedAssembly.FullName, dllPath);
                     ValidateAssemblyVersions(logger, referencedAssemblyName, loadedAssembly.GetName());
                     continue;
                 }
             }
             else
             {
-                logger.LogWarning("Referenced Assembly {AssemblyFullName} is not at {Path}", referencedAssemblyName.FullName, dllPath);
+                logger.LogWarning("Referenced Assembly \"{AssemblyFullName}\" was not found at \"{Path}\"", referencedAssemblyName.FullName, dllPath);
             }
 
             try
@@ -63,19 +65,20 @@ internal static class AssemblyLoader
             }
             catch (Exception e)
             {
-                logger.LogError(e, "Unable to load Assembly {AssemblyFullName}", referencedAssemblyName.FullName);
+                logger.LogError(e, "Unable to load Assembly \"{AssemblyFullName}\"", referencedAssemblyName.FullName);
                 loadedAssembly = null;
             }
 
             // .NET Framework found the Assembly and loaded it successfully
             if (loadedAssembly is not null)
             {
+                logger.LogDebug("Referenced Assembly was loaded by the .NET Framework: referenced: \"{ReferencedAssemblyName}\" loaded: \"{LoadedAssemblyName}\"", referencedAssemblyName.FullName, loadedAssembly.FullName);
                 ValidateAssemblyVersions(logger, referencedAssemblyName, loadedAssembly.GetName());
                 continue;
             }
 
             // nothing works so just throw
-            var exception = new Exception($"Missing Assembly {referencedAssemblyName.FullName}");
+            var exception = new Exception($"Missing Assembly \"{referencedAssemblyName.FullName}\"");
             logger.LogError(exception, null);
             throw exception;
         }
@@ -87,7 +90,12 @@ internal static class AssemblyLoader
         var actualVersion = actual.Version;
 
         if (expectedVersion.Major != actualVersion.Major)
-            LogThrow(logger, $"Version mismatch for Assembly {expected.Name}! expected: {expectedVersion} actual: {actualVersion}");
+            LogThrow(logger, $"Version mismatch for Assembly \"{expected.Name}\"! expected: {expectedVersion} actual: {actualVersion}");
+
+        if (!expectedVersion.Equals(actualVersion))
+        {
+            logger.LogWarning("Version mismatch for Assembly \"{AssemblyName}\": expected: {ExpectedVersion} actual: {ActualVersion}", expected.Name, expectedVersion, actualVersion);
+        }
     }
 
     private static void LogThrow(ILogger logger, string msg)
