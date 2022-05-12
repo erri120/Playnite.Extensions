eb33f50c8c1795a214b4d7915b7a3df6d5a903d1
erri120 <erri120@protonmail.com>
2022-05-12 13:57:04 +0200

    Update script

--- scripts/build.py
+++ scripts/build.py
@@ -33,6 +33,51 @@ def get_build_output_path(plugin_path: str) -> str:
     return result
 
 
+def copy_plugin(plugin: str, build_output_path: str):
+    output_path = os.path.abspath(sys.argv[2])
+    validate_path(output_path)
+
+    plugin_output_path = os.path.join(output_path, plugin)
+
+    if os.path.exists(plugin_output_path):
+        shutil.rmtree(plugin_output_path)
+
+    print(f"Copying build output files from {build_output_path} to {plugin_output_path}")
+    shutil.copytree(build_output_path, plugin_output_path)
+
+
+def pack_plugin(plugin: str, build_output_path: str):
+    output_path = os.path.abspath(sys.argv[2])
+    validate_path(output_path)
+
+    zip_output_path = os.path.join(output_path, f"{plugin}.zip")
+    print(f"Packing plugin {plugin} to {zip_output_path}")
+    with zipfile.ZipFile(zip_output_path, "w", zipfile.ZIP_LZMA) as myzip:
+        for root, dirs, files in os.walk(build_output_path):
+            for file in files:
+                myzip.write(os.path.join(root, file), file)
+
+
+def update_plugin_manifest(plugin: str, build_output_path: str):
+    new_version = sys.argv[2]
+
+    extension_file = os.path.join(build_output_path, "extension.yaml")
+    validate_path(extension_file, False)
+
+    print(f"Updating manifest of plugin {plugin} at {extension_file}")
+
+    lines = []
+    with open(extension_file, "r", encoding="UTF-8") as f:
+        for line in f:
+            if line.startswith("Version: "):
+                lines.append(f"Version: {new_version}\n")
+            else:
+                lines.append(line)
+
+    with open(extension_file, "w", encoding="UTF-8") as f:
+        f.writelines(lines)
+
+
 def main():
     if len(sys.argv) != 3:
         raise ValueError("Not enough arguments!")
@@ -49,42 +94,11 @@ def main():
         build_output_path = get_build_output_path(plugin_path)
 
         if mode == "copy":
-            output_path = os.path.abspath(sys.argv[2])
-            validate_path(output_path)
-
-            plugin_output_path = os.path.join(output_path, plugin)
-
-            if os.path.exists(plugin_output_path):
-                shutil.rmtree(plugin_output_path)
-
-            print(f"Copying build output files from {build_output_path} to {plugin_output_path}")
-            shutil.copytree(build_output_path, plugin_output_path)
+            copy_plugin(plugin, build_output_path)
         elif mode == "pack":
-            output_path = os.path.abspath(sys.argv[2])
-            validate_path(output_path)
-
-            zip_output_path = os.path.join(output_path, f"{plugin}.zip")
-            with zipfile.ZipFile(zip_output_path, "w", zipfile.ZIP_LZMA) as myzip:
-                for root, dirs, files in os.walk(build_output_path):
-                    for file in files:
-                        myzip.write(os.path.join(root, file), file)
+            pack_plugin(plugin, build_output_path)
         elif mode == "update":
-            new_version = sys.argv[2]
-
-            extension_file = os.path.join(build_output_path, "extension.yaml")
-            validate_path(extension_file, False)
-
-            lines = []
-            with open(extension_file, "r", encoding="UTF-8") as f:
-                for line in f:
-                    if line.startswith("Version: "):
-                        lines.append(f"Version: {new_version}\n")
-                    else:
-                        lines.append(line)
-
-            with open(extension_file, "w", encoding="UTF-8") as f:
-                f.writelines(lines)
-
+            update_plugin_manifest(plugin, build_output_path)
         else:
             raise ValueError(f"Unknown mode: {mode}")
 
