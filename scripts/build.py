bca64c0879aa4741a21aa1b9a5e4b5f0220c9b22
erri120 <erri120@protonmail.com>
2022-05-20 18:44:51 +0200

    Update build script

--- scripts/build.py
+++ scripts/build.py
@@ -1,31 +1,33 @@
-from encodings import utf_8
-import sys
 import errno
 import os
-import shutil
-import zipfile
-import yaml
 import re
+import shutil
+import sys
+import zipfile
 from datetime import datetime
+from encodings import utf_8
+from pathlib import Path
 
-configuration = "Debug"
-target = "net462"
+import yaml
+
+configuration = 'Debug'
+target = 'net462'
 
 plugins = [
-    "F95ZoneMetadata",
-    "DLSiteMetadata",
-    "FanzaMetadata",
-    "GameManagement"
+    'F95ZoneMetadata',
+    'DLSiteMetadata',
+    'FanzaMetadata',
+    'GameManagement'
 ]
 
-version_regex = re.compile(r"\d.\d.\d")
-package_search_string = "<PackageReference Include=\"PlayniteSDK\" Version=\""
+version_regex = re.compile(r'\d.\d.\d')
+package_search_string = '<PackageReference Include="PlayniteSDK" Version="'
 
 
-def validate_path(path: str, is_dir=True):
-    if not os.path.exists(path):
+def validate_path(path: Path, is_dir=True):
+    if not path.exists():
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
-    if is_dir and not os.path.isdir(path):
+    if is_dir and not path.is_dir():
         raise NotADirectoryError(errno.ENOTDIR, os.strerror(errno.ENOTDIR), path)
 
 
@@ -33,8 +35,8 @@ def decode_utf8(b) -> str:
     return utf_8.decode(b)[0]
 
 
-def get_build_output_path(plugin_path: str) -> str:
-    result = os.path.join(plugin_path, "bin", configuration, target)
+def get_build_output_path(plugin_path: Path) -> Path:
+    result = plugin_path.joinpath('bin', configuration, target)
     validate_path(result)
     return result
 
@@ -42,7 +44,7 @@ def get_build_output_path(plugin_path: str) -> str:
 def validate_version(version) -> str:
     regex_match = version_regex.match(version)
     if regex_match is None:
-        raise ValueError(f"Invalid Version: {version}")
+        raise ValueError(f'Invalid Version: {version}')
 
     return version
 
@@ -52,12 +54,12 @@ def parse_new_version() -> str:
     return validate_version(new_version)
 
 
-def get_playnite_version(csproj_path: str) -> str:
+def get_playnite_version(csproj_path: Path) -> str:
     validate_path(csproj_path, is_dir=False)
 
     package_version = None
-    with open(csproj_path, "r", encoding="UTF-8") as f:
-        for line in f:
+    with csproj_path.open('r', encoding='utf-8') as file:
+        for line in file:
             index = line.find(package_search_string)
             if index == -1:
                 continue
@@ -66,119 +68,119 @@ def get_playnite_version(csproj_path: str) -> str:
             break
 
     if package_version is None:
-        raise ValueError(f"Unable to find package reference in {csproj_path}")
+        raise ValueError(f'Unable to find package reference in {csproj_path}')
 
     return validate_version(package_version)
 
 
-def copy_plugin(plugin: str, build_output_path: str):
-    output_path = os.path.abspath(sys.argv[2])
+def copy_plugin(plugin: str, build_output_path: Path):
+    output_path = Path(os.path.abspath(sys.argv[2]))
     validate_path(output_path)
 
-    plugin_output_path = os.path.join(output_path, plugin)
+    plugin_output_path = output_path.joinpath(plugin)
 
-    if os.path.exists(plugin_output_path):
+    if plugin_output_path.exists():
         shutil.rmtree(plugin_output_path)
 
-    print(f"Copying build output files from {build_output_path} to {plugin_output_path}")
+    print(f'Copying build output files from {build_output_path} to {plugin_output_path}')
     shutil.copytree(build_output_path, plugin_output_path)
 
 
-def pack_plugin(plugin: str, build_output_path: str):
-    output_path = os.path.abspath(sys.argv[2])
+def pack_plugin(plugin: str, build_output_path: Path):
+    output_path = Path(os.path.abspath(sys.argv[2]))
     validate_path(output_path)
 
-    zip_output_path = os.path.join(output_path, f"{plugin}.pext")
-    print(f"Packing plugin {plugin} to {zip_output_path}")
-    with zipfile.ZipFile(zip_output_path, "w", zipfile.ZIP_DEFLATED) as myzip:
+    zip_output_path = output_path.joinpath(f'{plugin}.pext')
+    print(f'Packing plugin {plugin} to {zip_output_path}')
+    with zipfile.ZipFile(zip_output_path, 'w', zipfile.ZIP_DEFLATED) as myzip:
         for root, dirs, files in os.walk(build_output_path):
             for file in files:
                 myzip.write(os.path.join(root, file), file)
 
 
-def update_plugin_manifest(plugin: str, build_output_path: str, new_version: str):
-    extension_file = os.path.join(build_output_path, "extension.yaml")
+def update_plugin_manifest(plugin: str, build_output_path: Path, new_version: str):
+    extension_file = build_output_path.joinpath('extension.yaml')
     validate_path(extension_file, False)
 
-    print(f"Updating manifest of plugin {plugin} at {extension_file}")
+    print(f'Updating manifest of plugin {plugin} at {extension_file}')
 
-    with open(extension_file, "r", encoding="UTF-8") as f:
-        extension_manifest = yaml.safe_load(f)
+    with extension_file.open('r', encoding='utf-8') as file:
+        extension_manifest = yaml.safe_load(file)
 
-    extension_manifest["Version"] = new_version
+    extension_manifest['Version'] = new_version
 
-    with open(extension_file, "w", encoding="UTF-8") as f:
-        yaml.safe_dump(extension_manifest, f)
+    with extension_file.open('w', encoding='utf-8') as file:
+        yaml.safe_dump(extension_manifest, file)
 
 
 def update_installer_manifest(plugin: str, new_version: str, playnite_version: str):
-    manifest_path = os.path.join(os.path.abspath("manifests"), f"{plugin}.yaml")
+    manifest_path = Path(os.path.abspath('manifests')).joinpath(f'{plugin}.yaml')
     validate_path(manifest_path, is_dir=False)
 
-    with open(manifest_path, "r", encoding="UTF-8") as f:
-        installer_manifest = yaml.safe_load(f)
+    with manifest_path.open('r', encoding='utf-8') as file:
+        installer_manifest = yaml.safe_load(file)
 
-    packages = installer_manifest["Packages"]
+    packages = installer_manifest['Packages']
     if packages is not None:
-        existing_package = next((package for package in packages if package["Version"] == new_version), None)
+        existing_package = next((package for package in packages if package['Version'] == new_version), None)
         if existing_package is not None:
-            print(f"Package already exists for {plugin} v{new_version}")
+            print(f'Package already exists for {plugin} v{new_version}')
             return
     else:
-        installer_manifest["Packages"] = []
-        packages = installer_manifest["Packages"]
+        installer_manifest['Packages'] = []
+        packages = installer_manifest['Packages']
 
     release_date = datetime.now()
 
     new_package = {
-        "Version": new_version,
-        "RequiredApiVersion": playnite_version,
-        "ReleaseDate": release_date.strftime("%Y-%m-%d"),
-        "PackageUrl": f"https://github.com/erri120/Playnite.Extensions/releases/download/v{new_version}/{plugin}.pext"
+        'Version': new_version,
+        'RequiredApiVersion': playnite_version,
+        'ReleaseDate': release_date.strftime('%Y-%m-%d'),
+        'PackageUrl': f'https://github.com/erri120/Playnite.Extensions/releases/download/v{new_version}/{plugin}.pext'
     }
 
     packages.insert(0, new_package)
-    installer_manifest["Packages"] = packages
+    installer_manifest['Packages'] = packages
 
-    with open(manifest_path, "w", encoding="UTF-8") as f:
-        yaml.safe_dump(installer_manifest, f)
+    with manifest_path.open('w', encoding='utf-8') as file:
+        yaml.safe_dump(installer_manifest, file)
 
 
 def main():
     if len(sys.argv) != 3:
-        raise ValueError("Not enough arguments!")
+        raise ValueError('Not enough arguments!')
 
-    src_path = os.path.abspath("src")
+    src_path = Path(os.path.abspath('src'))
     validate_path(src_path)
 
     mode = sys.argv[1]
 
-    csproj_path = os.path.join(src_path, "Extensions.Common", "Extensions.Common.csproj")
+    csproj_path = src_path.joinpath('Extensions.Common', 'Extensions.Common.csproj')
     validate_path(csproj_path, is_dir=False)
 
-    if mode == "update":
+    if mode == 'update':
         playnite_version = get_playnite_version(csproj_path)
         new_version = parse_new_version()
     else:
-        playnite_version = ""
-        new_version = ""
+        playnite_version = ''
+        new_version = ''
 
     for plugin in plugins:
-        plugin_path = os.path.join(src_path, plugin)
+        plugin_path = src_path.joinpath(plugin)
         validate_path(plugin_path)
 
         build_output_path = get_build_output_path(plugin_path)
 
-        if mode == "copy":
+        if mode == 'copy':
             copy_plugin(plugin, build_output_path)
-        elif mode == "pack":
+        elif mode == 'pack':
             pack_plugin(plugin, build_output_path)
-        elif mode == "update":
+        elif mode == 'update':
             update_plugin_manifest(plugin, build_output_path, new_version)
             update_installer_manifest(plugin, new_version, playnite_version)
         else:
-            raise ValueError(f"Unknown mode: {mode}")
+            raise ValueError(f'Unknown mode: {mode}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
